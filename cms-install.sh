8ad548f577d8485c777dea48aad96e33edae7530
Pasit Sangprachathanarak <ouipingpasitsang@gmail.com>
2025-06-28 16:04:44 +0700

    Update cms-install.sh

--- cms-install.sh
+++ cms-install.sh
@@ -62,7 +62,7 @@ sudo -u postgres psql --username=postgres --dbname="$PG_DB" --command="ALTER SCH
 sudo -u postgres psql --username=postgres --dbname="$PG_DB" --command="GRANT SELECT ON pg_largeobject TO \"$PG_USER\";"
 NEW_URL="database = \"postgresql+psycopg2://$ESC_USER:$ESC_PASS@localhost:5432/$ESC_DB\""
 sudo chmod 640 /usr/local/etc/cms.toml
-sudo chown root:cmsuser /usr/local/etc/cms.toml
+sudo chown $CUR_USER:$CUR_USER /usr/local/etc/cms.toml
 sudo sed -i "s|^database = \".*\"|$NEW_URL|" "$CONFIG_PATH"
 sudo sed -i "s|^secret_key = \".*\"|secret_key = \"$SECRET_KEY\"|" "$CONFIG_PATH"
 $CUR_DIR/cms_venv/bin/cmsInitDB
@@ -120,7 +120,7 @@ WantedBy=multi-user.target
 EOF
 
 sudo chmod 640 "$CUR_DIR/resource-service.conf"
-sudo chown root:cmsuser "$CUR_DIR/resource-service.conf"
+sudo chown $CUR_USER:$CUR_USER "$CUR_DIR/resource-service.conf"
 
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
